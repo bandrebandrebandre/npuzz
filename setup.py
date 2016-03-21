d8a6227bc58343b8030383417fd7c6b8d60bbffa
Ben Andre <bandre@gmail.com>
2016-03-21 00:49:19 -0400

    Remove reference testing requirements from setup.py

--- setup.py
+++ setup.py
@@ -11,9 +11,6 @@ setup(name='npuzz',
       include_package_data=True,
       zip_safe=False,
       install_requires=requires,
-      extras_require={
-          'testing': testing_extras,
-      },
       entry_points="""\
       [console_scripts]
       run_search = npuzz.scripts.run_search:main
